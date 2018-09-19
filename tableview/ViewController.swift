fdd48cc1698c0a86275bb2c7b98124c78f71589a
Alumno <ulsa@iMac-de-Alumno.local>
2018-09-18 18:45:03 -0700

    tablasss

--- tableview/ViewController.swift
+++ tableview/ViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    let alumnos = ["Rene","Minneth","Betin","David","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno","Relleno"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) ->Int {
+        return alumnos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->UITableViewCell{
+        let CeldaAlumno = tableView.dequeueReusableCell(withIdentifier: "cellAlumno"  )
+        CeldaAlumno?.textLabel?.text = alumnos[indexPath.row]
+        
+        return CeldaAlumno!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
